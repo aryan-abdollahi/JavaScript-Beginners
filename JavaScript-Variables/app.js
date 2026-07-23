2cc737e9571848d9d51f9a79b609b1faa8f2ffa5
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-07-23 16:53:07 +0430

    JavaScript-Variables-practice

---
+++ JavaScript-Variables/app.js
@@ -0,0 +1,48 @@
+// مینی پروژه
+
+// let firstName = "Ali";
+
+// let lastName = "Ahmadi";
+
+// let age = 23;
+
+// let city = "Tehran";
+
+// console.log(firstName,lastName,age,city);
+
+// تمرین اول
+// let name = "Ariyan";
+
+// let age = 22;
+
+// let city = "Tehran";
+
+// let job = "Realstate";
+
+// console.log(name, age, city, job);
+
+//تمرین دوم
+
+
+
+// let productName = "Xbox";
+
+// let price = "$500";
+
+// let disCount = "tenperson";
+
+// let stock = 100;
+
+// console.log(productName, price, disCount, stock);
+
+//تمرین سوم
+
+// خروجی 30 است
+
+//تمرین چهارم
+//  باید استفاده شود let چون از متغیر
+// let year = 2025;
+
+// year = 2026;
+
+// console.log(year)
