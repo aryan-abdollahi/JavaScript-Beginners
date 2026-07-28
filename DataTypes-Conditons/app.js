a472c99fbb63167cf4c783162dadbf3790012547
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-07-28 18:33:54 +0430

    Data Type Conditions

---
+++ DataTypes-Conditons/app.js
@@ -0,0 +1,55 @@
+// مینی پروژه
+// let username = "admin";
+
+// let password = 1234;
+
+//   if ( username === "admin" && password === 10000)
+// {
+// console.log("Login Success");
+// }
+// else{
+
+// console.log("Login failed")
+// }
+// 
+// تمرین 1
+
+// let temperature = 35;
+
+// if (temperature > 30) {
+
+//   console.log("hot");
+// } else {
+
+//   console.log("Normal");
+// }
+// تمرین2
+
+// let money = 500;
+
+// if ( money > 1000){
+
+// console.log("Can Buy");
+// } else{
+// console.log("Not Enough money");
+// }
+
+// تمرین 3
+
+// let name = "Aryian";
+
+// let score = 50;
+
+// if (score >=60){
+
+//     console.log("passed")
+
+// } else{
+//     console.log("failed")
+// }
+
+// چالش خانه
+
+let age = 65;
+
+console.log
