8969f7a9deb45eae39a875c5a122c6a70427d1be
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-07-31 14:27:11 +0430

    Calculator project

---
+++ Calculator-project/app.js
@@ -0,0 +1,33 @@
+تمرین1
+let num1 = 20;
+
+let num2 = 10;
+
+let operator = "+";
+
+if (operator === "+") {
+  console.log(num1 +num2);
+} else if (operator === "-") console.log(num1 - num2);
+
+تمرین2
+let num1 = 10;
+
+let num2 = 2;
+
+let operator = "+";
+{
+  if (operator === "+") console.log(num1 + num2);
+  else if (operator === "-") console.log(num1 - num2);
+  else if (operator === "*") console.log(num1 * num2);
+  else if (operator === "/") console.log(num1 / num2);
+}
+تمرین3
+
+let num1 = 15;
+
+let num2 = 3;
+
+let operator = "/";
+
+if (operator === "/") console.log(num1 / num2);
+
