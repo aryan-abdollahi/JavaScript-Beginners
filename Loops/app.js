402a3c085f97c413b6d9c805d5b7556fa467af8b
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-08-02 16:10:21 +0430

    Loops-practice

---
+++ Loops/app.js
@@ -0,0 +1,35 @@
+// مینی پروژه کلاس
+
+// for(let i=1;i<=10;i++){
+
+//     console.log(5*i);
+// }
+
+// تمرین خانه 1
+
+// for(let i=1;i<=14;i++){
+
+//     console.log(7*i);
+// }
+
+// تمرین خانه 2
+
+// let sum =0;
+
+// for(let i=1;i<=100;i++){
+//     sum = sum + i;
+//     console.log(sum);
+// }
+
+// تمرین خانه 3
+
+// for(let i=2;i<=50;i+=2){
+
+//     console.log(i);
+// }
+
+// تمرین 4 خانه
+
+// for (let i = 2; i => 50; i += 2) {
+//   console.log(i);
+// }
