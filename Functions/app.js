80c8336e270d75232bb580ce6069b8609c3e12fa
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-08-04 19:17:07 +0430

    functions

---
+++ Functions/app.js
@@ -0,0 +1,29 @@
+مینی پروژه کلاس
+
+function calculate (a,b,operator) {
+
+    if(operator === "+") {
+        return a+b;
+    }
+
+    if(operator === "-") {
+        return a-b;
+    }
+
+    if(operator === "*") {
+        return a*b;
+    }
+
+    if(operator === "/")
+        return a/b;
+}
+    console.log(calculate(10,5,"+"));
+
+تمرین اول
+
+function square(number) {
+  console.log(number);
+}
+square(5);
+multiply = 5*5;
+console.log(multiply)
