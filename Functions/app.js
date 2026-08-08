9fc43e17c3bb9571a4b9fc1d6d8acbdf715c88e3
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-08-08 19:46:21 +0430

    functions-practice

--- Functions/app.js
+++ Functions/app.js
@@ -1,29 +1,58 @@
-مینی پروژه کلاس
+// مینی پروژه کلاس
 
-function calculate (a,b,operator) {
+function calculate(a, b, operator) {
+  if (operator === "+") {
+    return a + b;
+  }
 
-    if(operator === "+") {
-        return a+b;
-    }
+  if (operator === "-") {
+    return a - b;
+  }
 
-    if(operator === "-") {
-        return a-b;
-    }
+  if (operator === "*") {
+    return a * b;
+  }
 
-    if(operator === "*") {
-        return a*b;
-    }
-
-    if(operator === "/")
-        return a/b;
+  if (operator === "/") return a / b;
 }
-    console.log(calculate(10,5,"+"));
+console.log(calculate(10, 5, "+"));
 
-تمرین اول
+// تمرین اول
 
 function square(number) {
-  console.log(number);
+  return number ** 2;
 }
-square(5);
-multiply = 5*5;
-console.log(multiply)
+const result1 = square(5);
+
+console.log(result);
+
+// تمرین 2
+
+function cube(number) {
+  return number ** 3;
+}
+const result = cube(3);
+
+console.log(result);
+
+//تمرین 3
+
+function checkTemp(temp) {
+  if (temp >= 30) {
+    console.log("hot");
+  } else {
+    console.log("normal");
+  }
+}
+
+checkTemp(29);
+
+// تمرین4
+
+function calculatesalary(hours, rate) {
+  const multiply = 20 * 15;
+
+  console.log(multiply);
+}
+
+calculatesalary();
--- Scope/app.js
+++ Scope/app.js
@@ -1,60 +1,9 @@
-// مینی پروژه
+// function square(number) {
+//   square(5);
 
-let website = "My Shop";
+//   return;
 
-function showUser() {
+//   console.log(square);
+// }
 
-    let Username = "Ali";
-    let age = 24;
-
-    console.log("Website:", website);
-    console.log("Username:",username);
-    console.log("Age:",age);
-}
-
-showUser();
-
-// تمرین 1
-
-let Company = "Open AI";
-
-function  Global() {
-
-    console.log(Company);
-    
-}
-
-Global();
-
-// تمرین 2
-
-let salary = 5000;
-
-function showsalary() {
-
-    
-
-}
-
-console.log(salary);
-
-// showsalary();
-// تمرین 3
-
-for(let i=1;i<=3;i++) {
-
-}
-console.log(i);
-// هیچی چاپ نمیشه چون بیرون متغیر چاپ شده
-// و i داخل حلقه قرار داره
-// تمرین4
-
-if (true) {
-    let y =7;
-    var x=4;
-    console.log(y);
-}
-// console.log(x);
-// let همیشه داخل متعیر خروجی می دهد در غیر اینصورت بیرون متغیر Refrence error می دهد
-// var بیرون متغیر خروجی می دهد
-
+// square(number);
