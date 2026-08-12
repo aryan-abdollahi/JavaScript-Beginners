030e5118003e13360eaa11a4e2e3727c6c83c5b4
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-08-12 19:14:50 +0430

    DOM

---
+++ DOM/app.js
@@ -0,0 +1,25 @@
+تمرین 1
+const h1 = document.querySelector("h1");
+
+h1.innerHTML = "messi"
+
+const p = document.querySelector("p");
+
+p.textContent = "He plays for inter Miami"
+
+تمرین 2
+const h1 = document.querySelector("h1");
+
+h1.style.color = "red";
+
+h1.style.fontSize = "2rem";
+
+h1.style.backgroundColor = "blue"
+
+h1.style.padding = "12px"
+
+تمرین 3
+
+const p =document.querySelector("p1,p2,p3");
+
+p.innerHTML = "salam"
--- Scope/app.js
+++ Scope/app.js
@@ -1,9 +1,77 @@
-// function square(number) {
-//   square(5);
+// تمرین 1
 
-//   return;
+// function Global() {
 
-//   console.log(square);
+//     let company = "Open AI"
+
+//     console.log(company);
 // }
 
-// square(number);
+//  Global();
+
+// تمرین 2
+
+// function salary(){
+
+//     var salary = 5000;
+// }
+
+// console.log(salary);
+
+// salary();
+// چون بیرون تابع چاپ شده
+
+// تمرین 3
+
+// for(var i=1;i<=3;i++){
+
+// }
+
+// console.log(i);
+// اگر متغیر let بدیم refrence error میده چون بیرون متغیر چاپ شده اما اگر var بدیم خروجی 4 چاپ می شود
+
+// تمرین 4
+
+//   if(true){
+
+//     let i0=1;
+
+//     var i=1;
+
+//   }
+// اگر با var تعریف کنیم بیرون تابع چاپ می شود
+
+// ولی اگر با let تعریف کنیم داخل تابع فقط چاپ می شود
+
+//   console.log(i);
+
+//   console.log(i0);
+
+// چالش خانه
+
+// function calculateArea(width, height) {
+//   let area = width * height;
+
+//   return area;
+// }
+
+// console.log(calculateArea(20, 20));
+
+// پروژه پایان جلسه
+
+// let shopName = "Teach Store";
+
+// function createOrder(product,quality) {
+
+//   let orderld = 1001;
+
+//   console.log("shop:", shopName);
+
+//   console.log("product:", product);
+
+//   console.log("Quantity:", quality);
+
+//   console.log("Order ID:", orderld);
+// }
+
+// createOrder("Laptop",2);
