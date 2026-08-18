2f21f18a3f27ab645437ace6eceab12c09116199
Aryan Abdollahi <arianabdollahi82@gmail.com>
2026-08-18 19:13:02 +0430

    Event

--- Event/App.js
+++ Event/App.js
@@ -1,18 +1,25 @@
 // مینی پروژه کلاس
 
-const input = document.querySelector("input");
-const button = document.querySelector("button");
-const title = document.querySelector("h2");
+// const input = document.querySelector("input");
+// const button = document.querySelector("button");
+// const title = document.querySelector("h2");
 
-button.addEventListener("click",()=>{
+// button.addEventListener("click",()=>{
 
-    title.textContent = input.value;
+//     title.textContent = input.value;
 
-    title.style.color = "blue";
+//     title.style.color = "blue";
 
-})
+// })
 
 // تمرین 1
 
+const input = document.querySelector("input");
 
+const button = document.querySelector("button");
 
+const title = document.querySelector("h1");
+
+button.addEventListener("click",()=> {
+  title.textContent = input.value;
+});
